9d2ac054904c77cfd823211d8c19769f63b20127
Pheniksja <kmadalinska@gmail.com>
2020-12-01 16:49:30 +0100

    changed methods name in MainApp

--- src/MainApp.java
+++ src/MainApp.java
@@ -2,9 +2,9 @@ public class MainApp {
     public static void main(String[] args) {
 
         Bug firstBug= new Bug( "Bug description: The bug with field validation: telephonenumber", "kasia@email.pl",1);
-        firstBug.getAllInformationAboutBug();
-        firstBug.emailPersonReportingBug();
-        firstBug.statusBug();
+        firstBug.showAllInformationAboutBug();
+        firstBug.showEmailPersonReportingBug();
+        firstBug.showStatusBug();
         System.out.println("The priority of bug is: " + firstBug.getPriorityBug());
 
     }
