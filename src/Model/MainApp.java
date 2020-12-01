e9e5b8478ffa7d3e261da8154767fbb3e7dd21b7
Pheniksja <kmadalinska@gmail.com>
2020-12-01 19:22:09 +0100

    added exercises3

---
+++ src/Model/MainApp.java
@@ -0,0 +1,34 @@
+package Model;
+
+
+public class MainApp {
+    public static void main(String[] args) {
+
+        Bug firstBug= new Bug( "The bug with field validation: telephonenumber", "kasia@email.pl",1);
+        //  firstBug.showAllInformationAboutBug();
+
+        //Bug Description
+        System.out.println(firstBug.getBugDescription());
+        firstBug.setBugDescription("Error");
+        System.out.println(firstBug.getBugDescription());
+
+        firstBug.setBugDescription("Error telephonenumber");
+        System.out.println(firstBug.getBugDescription());
+
+        // e-mail
+        System.out.println(firstBug.getEmailPersonReportingBug());
+        firstBug.setEmailPersonReportingBug("kasiaAtasia.pl");
+        System.out.println(firstBug.getEmailPersonReportingBug());
+
+        //priority Bug
+        System.out.println(firstBug.getPriorityBug());
+        firstBug.setPriorityBug(0);
+        System.out.println(firstBug.getPriorityBug());
+
+        firstBug.setPriorityBug(-6);
+        System.out.println(firstBug.getPriorityBug());
+
+        firstBug.setPriorityBug(4);
+        System.out.println(firstBug.getPriorityBug());
+    }
+}
